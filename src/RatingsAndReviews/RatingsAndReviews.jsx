67f259a3f4636284c89c7b77f49676714cfc415f
Joe Shultz josephcshultz@gmail.com <josephcshultz@gmail.com>
2022-05-20 15:24:39 -0600

    Rating and Reviews main component created

---
+++ src/RatingsAndReviews/RatingsAndReviews.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class RatingsAndReviews extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <div>Rating Breakdown</div>
+        <div>Review List</div>
+      </div>
+    );
+  }
+}
+
+export default RatingsAndReviews;
