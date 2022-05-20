48da18464fcbb39d7c333f874b9e7509b564a0b8
Yanick <yanick@bu.edu>
2022-05-20 10:27:44 -0500

    save

--- src/QuestionsAndAnswers/QuestionList.jsx
+++ src/QuestionsAndAnswers/QuestionList.jsx
@@ -1,1 +1,22 @@
 import React from 'react';
+import Modal from 'react-modal';
+
+class QuestionList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  componentDidMount() {
+
+  };
+
+  render () {
+    return (
+      <div>QUESTIONS LIST MIC CHECK</div>
+    )
+  };
+
+}
