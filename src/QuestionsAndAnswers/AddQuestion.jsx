15472e9b262eb806b19015ebd8b46672377b3795
Yanick <yanick@bu.edu>
2022-05-24 15:17:49 -0500

    form css looks decent

--- src/QuestionsAndAnswers/AddQuestion.jsx
+++ src/QuestionsAndAnswers/AddQuestion.jsx
@@ -10,6 +10,19 @@ class AddQuestion extends React.Component {
     };
 
     this.closeModal = this.closeModal.bind(this);
+    this.validate = this.validate.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const isValid = this.validate;
+  }
+
+  validate() {
+    let questionError = '';
+    let nicknameError = '';
+    let emailError = '';
   }
 
   closeModal() {
@@ -24,11 +37,14 @@ class AddQuestion extends React.Component {
     return (
       <Modal isOpen={modalOpen} appElement={document.getElementById('root')}>
         <div className="formContainer">
+          <div className="x" onClick={this.closeModal}>x</div>
           <h1>ASK YOUR QUESTION</h1>
           <h3>{`About the ${item.name}`}</h3>
-          <textarea rows="6" cols="60" placeholder="Your Question..." />
+          <div className="questionDiv">
+            <textarea rows="6" cols="60" placeholder="*Your Question..." />
+          </div>
           <div className="nicknameDiv">
-            What is your nickname?
+            *What is your nickname?
             <input id="nickname" placeholder="Example: jackson11!" />
             <p className="privacy">For privacy reasons, do not use your full name or email address</p>
           </div>
@@ -36,8 +52,14 @@ class AddQuestion extends React.Component {
             *What is your email?
             <input id="email" placeholder="example@outlook.com" />
           </div>
-
-          <button className="button" type="button" onClick={this.closeModal}>CLOSE</button>
+          <div className="review">
+            What did you like or dislike about the product?
+            <textarea rows="6" cols="60" placeholder="Your thoughts..." />
+          </div>
+          <div>
+            <button className="button" type="button" onClick={this.closeModal}>CLOSE</button>
+            <button className="button" type="button">SUBMIT</button>
+          </div>
         </div>
       </Modal>
     );
